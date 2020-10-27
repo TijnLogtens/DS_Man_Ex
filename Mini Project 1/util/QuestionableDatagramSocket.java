be0aebbbeee1b5ee6550979a9d15be01bab805c2
Fabio Barbero <fabio.barbero@student.maastrichtuniversity.nl>
2020-10-27 09:36:26 +0100

    Change folder name to match style

---
+++ Mini Project 1/util/QuestionableDatagramSocket.java
@@ -0,0 +1,75 @@
+package util;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.*;
+
+public class QuestionableDatagramSocket extends DatagramSocket {
+
+    public QuestionableDatagramSocket(int port) throws SocketException {
+        super(port);
+    }
+
+    /***
+     *
+     * @param packets
+     * @return Number of send packets
+     * @throws IOException
+     */
+    public int send(DatagramPacket[] packets) throws IOException {
+        Random r = new Random();
+        int sendMessagesCounter = 0;
+        switch (r.nextInt(4)) {
+            case 0: // discard
+                System.out.println("discard");
+                boolean discard_first_msg = true;
+                for (DatagramPacket p :
+                        packets) {
+                    if (discard_first_msg) discard_first_msg = false;
+                    else if (r.nextInt(2) == 1) {
+                        super.send(p);
+                        sendMessagesCounter++;
+                    }
+                }
+                break;
+            case 1: // duplicate
+                System.out.println("duplicate");
+                boolean duplicate_first_msg = true;
+                for (DatagramPacket p :
+                        packets) {
+                    if (duplicate_first_msg) {
+                        super.send(p);
+                        duplicate_first_msg = false;
+                        sendMessagesCounter++;
+                    }
+                    if (r.nextInt(2) == 1) {
+                        super.send(p);
+                        sendMessagesCounter++;
+                    }
+                    super.send(p);
+                    sendMessagesCounter++;
+                }
+                break;
+            case 2: // reorder
+                System.out.println("normal or reorder");
+                List<DatagramPacket> packetslist = Arrays.asList(packets.clone());
+                Collections.shuffle(packetslist);
+                for (DatagramPacket p : packetslist) {
+                    super.send(p);
+                    sendMessagesCounter++;
+                }
+                break;
+            case 3:
+                System.out.println("normal");
+                for (DatagramPacket p : packets) {
+                    super.send(p);
+                    sendMessagesCounter++;
+                }
+
+        }
+        return sendMessagesCounter;
+
+    }
+}
--- tmp2/src/MessageQueue.java
+++
@@ -1,9 +0,0 @@
-import java.beans.PropertyChangeListener;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MessageQueue extends Remote {
-    void put(String message) throws RemoteException;
-    String get(int id) throws RemoteException;
-    int register() throws RemoteException;
-}
