e218a3e98901ec68fc544f252ccfefcd8f1921fe
Rudolf Rosenfeld <iduras3@users.noreply.github.com>
2020-10-27 09:52:05 +0100

    update clients

--- Project3/src/client/GetClient.java
+++ Project3/src/client/GetClient.java
@@ -4,16 +4,19 @@ import util.Message;
 import util.MessageType;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class GetClient {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         String ip = args[0];
         int port = Integer.parseInt(args[1]);
         int key = Integer.parseInt(args[2]);
         Message m = new Message(MessageType.GET,key,"");
         Socket con = new Socket(ip, port);
+        new ObjectOutputStream(con.getOutputStream()).writeObject(m);
 
     }
 }
--- Project3/src/client/PutClient.java
+++ Project3/src/client/PutClient.java
@@ -4,7 +4,6 @@ import util.Message;
 import util.MessageType;
 
 import java.io.IOException;
-import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
@@ -17,6 +16,7 @@ public class PutClient {
         String value = args[3];
         Message m = new Message(MessageType.PUT,key,value);
         Socket con = new Socket(ip, port);
+        new ObjectOutputStream(con.getOutputStream()).writeObject(m);
     }
 }
 
